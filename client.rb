f26e50f42ea6fa9c01f34f64dc7f35ea473620f2
Masauki Takahashi <masayuki038@gmail.com>
2012-04-26 01:11:14 +0900

    To avoid a busy loop after closing a socket in client.

--- client.rb
+++ client.rb
@@ -5,8 +5,10 @@ require 'socket'
 s = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM, 0)
 sockaddr = Socket.sockaddr_in(8000, '127.0.0.1')
 s.connect(sockaddr)
+sleep 5
 s.write "Hello,"
 puts "Received #{s.recv(1024)}"
+sleep 5
 s.write "world."
 puts "Received #{s.recv(1024)}"
 
--- src/main/java/net/wrap_trap/example/nio/EchoServer.java
+++ src/main/java/net/wrap_trap/example/nio/EchoServer.java
@@ -7,17 +7,13 @@ import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
-import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Set;
 
 public class EchoServer {
 
     private ServerSocketChannel serverChannel;
     private Selector selector;
-
-    private List<ByteBuffer> buffers = new ArrayList<ByteBuffer>();
 
     public EchoServer() throws IOException {
         serverChannel = ServerSocketChannel.open();
@@ -37,30 +33,28 @@ public class EchoServer {
                 Set<SelectionKey> keys = selector.selectedKeys();
                 for (Iterator<SelectionKey> it = keys.iterator(); it.hasNext();) {
                     SelectionKey key = it.next();
+                    it.remove();
 
                     if (key.isAcceptable()) {
                         System.out.println("accept");
                         SocketChannel channel = serverChannel.accept();
                         channel.configureBlocking(false);
                         channel.register(key.selector(), SelectionKey.OP_READ);
-                    } else if (key.isReadable()) {
+                    } else {
                         SocketChannel channel = (SocketChannel) key.channel();
-                        System.out.println("read");
-                        ByteBuffer buffer = ByteBuffer.allocate(4096);
-                        if (channel.read(buffer) > 0) {
-                            buffer.flip();
-                            buffers.add(buffer);
-                            key.interestOps(SelectionKey.OP_READ | SelectionKey.OP_WRITE);
+                        if (key.isReadable()) {
+                            System.out.println("read");
+                            ByteBuffer buffer = ByteBuffer.allocate(4096);
+                            int len = channel.read(buffer);
+                            if (len > 0) {
+                                buffer.flip();
+                                channel.write(buffer);
+                            } else if (len == -1) {
+                                key.cancel();
+                                channel.close();
+                            }
                         }
-                    } else if (key.isWritable()) {
-                        System.out.println("write");
-                        SocketChannel channel = (SocketChannel) key.channel();
-                        for (ByteBuffer buffer : buffers) {
-                            channel.write(buffer);
-                        }
-                        key.interestOps(SelectionKey.OP_READ);
                     }
-                    it.remove();
                 }
             }
         } finally {
